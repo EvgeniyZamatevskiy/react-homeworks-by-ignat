0a3d3ea86a38afa84d770734dd91e4b4487e533c
EvgeniyZaM <jerry.zam.999@gmail.com>
2022-04-07 13:55:21 +0300

    homework 8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,26 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import { PeopleType } from "../HW8"
+
+type sortUpActionType = {
+    type: 'sort'
+    payload: 'up' | 'down'
+}
+
+type checkActionType = {
+    type: 'check'
+    payload: number
+}
+
+type PeopleReducerType = sortUpActionType | checkActionType
+
+export const homeWorkReducer = (state: Array<PeopleType>, action: PeopleReducerType): Array<PeopleType> => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            return action.payload === 'up' ? [...state].sort((a, b) => a.name > b.name ? 1 : -1) : [...state].sort((a, b) => a.name < b.name ? 1 : -1)
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter((s) => s.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
