63f9da89b84aee83f67baaef58e8fae86f40bfe0
EvgeniyZaM <jerry.zam.999@gmail.com>
2022-04-25 11:36:58 +0300

    homework 11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
@@ -8,30 +8,31 @@ function HW11() {
 
     return (
         <div>
-            <hr/>
+            <hr />
             homeworks 11
 
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+                <SuperRange onChangeRange={setValue1} value={value1} />
             </div>
 
             <div>
-                <span>{value1}</span>
                 <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
+                    onChangeOneRange={setValue1}
+                    onChangeTwoRange={setValue2}
+                    value1={value1}
+                    value2={value2}
+                    min={0}
+                    max={100}
                 />
-                <span>{value2}</span>
             </div>
 
-            <hr/>
+            <hr />
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperRange/>*/}
             {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
+            <hr />
         </div>
     )
 }
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,88 @@
-import React from 'react'
+import React, { ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, useCallback, useEffect, useRef } from 'react'
+import s from './SuperDoubleRange.module.css'
 
-type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
+type SuperDoubleRangePropsType = Omit<DefaultInputPropsType, 'type'> & {
+    onChangeOneRange?: (value: number) => void
+    onChangeTwoRange?: (value: number) => void
+    value1: number
+    value2: number
+    min: number
+    max: number
 }
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
+const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = ({ onChange, onChangeOneRange, onChangeTwoRange, value1, value2, min, max }) => {
+
+    const minValRef = useRef(min)
+    const maxValRef = useRef(max)
+    const range = useRef<HTMLDivElement>(null)
+
+    const getPercent = useCallback((value) => {
+        return Math.round(((value - min) / (max - min)) * 100)
+    }, [min, max])
+
+    useEffect(() => {
+        const minPercent = getPercent(value1)
+        const maxPercent = getPercent(maxValRef.current)
+
+        if (range.current) {
+            range.current.style.left = `${minPercent}%`
+            range.current.style.width = `${maxPercent - minPercent}%`
+        }
+    }, [value1, getPercent])
+
+    useEffect(() => {
+        const minPercent = getPercent(minValRef.current)
+        const maxPercent = getPercent(value2)
+
+        if (range.current) {
+            range.current.style.width = `${maxPercent - minPercent}%`
+        }
+    }, [value2, getPercent])
+
+    const onChangeOneRangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e)
+
+        const value = Math.min(Number(e.currentTarget.value), value2 - 1)
+        onChangeOneRange && onChangeOneRange(value)
+        minValRef.current = value
     }
-) => {
-    // сделать самому, можно подключать библиотеки
+
+    const onChangeTwoRangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e)
+
+        const value = Math.max(Number(e.currentTarget.value), value1 + 1)
+        onChangeTwoRange && onChangeTwoRange(value)
+        maxValRef.current = value
+    }
 
     return (
-        <>
-            DoubleRange
-        </>
+        <div className={s.superDoubleRange}>
+            <input
+                type="range"
+                value={value1}
+                min={min}
+                max={max}
+                onChange={onChangeOneRangeHandler}
+                className={`${s.thumb} ${s.thumbLeft}`}
+            />
+            <input
+                type="range"
+                value={value2}
+                min={min}
+                max={max}
+                onChange={onChangeTwoRangeHandler}
+                className={`${s.thumb} ${s.thumbRight}`}
+            />
+
+            <div className={s.slider}>
+                <div className={s.sliderTrack} />
+                <div ref={range} className={s.sliderRange} />
+                <div className={s.sliderLeftValue}>{value1}</div>
+                <div className={s.sliderRightValue}>{value2}</div>
+            </div>
+        </div>
     )
 }
 
